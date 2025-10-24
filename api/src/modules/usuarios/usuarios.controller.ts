5a666768a8cdf1ed514e85ec44bab3a6fb0bac95
JoaoVitor615 <jvpeniza@gmail.com>
2025-10-24 01:22:43 -0300

    feat: Implementa CRUD para usuários

--- api/src/modules/usuarios/usuarios.controller.ts
+++ api/src/modules/usuarios/usuarios.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsuarioService } from './usuarios.service';
+import UsuarioDTO from './dto/usuario.dto';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuarioService) {}
 
-  @Get('listar_todos')
+  @Get('listar-todos')
   async listar(){
     return await this.usuariosService.CarregarTodos();
   }
+
+  @Post('gravar')
+  async Criar(@Body() data: UsuarioDTO) {
+    return await this.usuariosService.Gravar(data);
+  }
+
+  @Get('deletar/:id')
+  async Deletar(@Param('id') id: number) {
+    return await this.usuariosService.Deletar(id);
+  }
+
+  @Get('carregar-por-hash/:hash')
+  async CarregarPorHash(@Param('hash') hash: string) {
+    return await this.usuariosService.CarregarPorHash(hash);
+  }
+
 }
