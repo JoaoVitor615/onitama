8a767d7db6d852c2439d1837389a3eca904318f5
Gabriel Prezzoti <gabrielbprezzoti@outlook.com>
2025-11-12 19:56:36 -0300

    feat: enable LAN access for development by configuring host and CORS

--- onitama-front/vite.config.js
+++ onitama-front/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
