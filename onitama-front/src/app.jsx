6e59861db96ae5a4641b94f963ae1cebd0e4274b
JoaoVitor615 <jvpeniza@gmail.com>
2025-11-14 23:08:26 -0300

    feat(auth): add authentication requirement for protected routes

--- onitama-front/src/app.jsx
+++ onitama-front/src/app.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import Menu from "./pages/menu";
 import Loja from "./pages/loja";
@@ -8,19 +8,32 @@ import Salas from "./pages/salas";
 //import About from "./pages/About";
 import "./App.module.css";
 
+function RequireAuth({ children }) {
+  const location = useLocation();
+  try {
+    const uid = localStorage.getItem("usuario_id");
+    const hash = localStorage.getItem("usuario_hash");
+    const hasAuth = Boolean(uid) && Boolean(hash);
+    if (!hasAuth) {
+      return <Navigate to="/" replace state={{ from: location }} />;
+    }
+  } catch (_) {}
+  return children;
+}
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/loja" element={<Loja />} />
-        <Route path="/itens" element={<Itens />} />
-        <Route path="/onitama" element={<Onitama />} />
-        <Route path="/salas" element={<Salas />} />
+        <Route path="/menu" element={<RequireAuth><Menu /></RequireAuth>} />
+        <Route path="/loja" element={<RequireAuth><Loja /></RequireAuth>} />
+        <Route path="/itens" element={<RequireAuth><Itens /></RequireAuth>} />
+        <Route path="/onitama" element={<RequireAuth><Onitama /></RequireAuth>} />
+        <Route path="/salas" element={<RequireAuth><Salas /></RequireAuth>} />
         {/* Adicione mais rotas conforme necessário */}
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<RequireAuth><ForgotPassword /></RequireAuth>} />
+        <Route path="/register" element={<RequireAuth><Register /></RequireAuth>} />
         {/* Garantir que rotas do Godot não sejam interceptadas */}
       </Routes>
     </div>
